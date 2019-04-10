9c1d224b2b001c2bc3c1e23c054b5a452249c501
Kagiso Leso <percykgs2@gmail.com>
2019-04-10 15:46:46 +0200

    Initial Commit

---
+++ DtApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DtApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DtApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+        
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DtApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DtApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
